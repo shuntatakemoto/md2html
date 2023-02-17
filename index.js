8f5bc522f72ffe182352665941268dca3f59efe4
haruta <58549977+shuntatakemoto@users.noreply.github.com>
2023-02-17 18:58:46 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const path=require("path");
-const fs=require("fs");
 const yargs=require("yargs/yargs");
 const {hideBin}=require("yargs/helpers");
+const {getPackageName}=require("./lib/name");
+const {readMarkdownFileSync}=require("./lib/file");
 
 const {argv}=yargs(hideBin(process.argv)).option("name",{describe:"CLI名を表示"}).option("file",{describe:"Markdownのパス"});
 
 if(argv.name){
-    const packageStr=fs.readFileSync(path.resolve(__dirname,"package.json"),{encoding:"utf-8"});
-    const package=JSON.parse(packageStr);
-    console.log(package.name);
+   const name=getPackageName();
+    console.log(name);
     process.exit(0);
 }
 
-const markdownStr=fs.readFileSync(path.resolve(__dirname,argv.file),{encoding:"utf-8"});
+const markdownStr=readMarkdownFileSync(path.resolve(__dirname,argv.file));
 console.log(markdownStr);
 
 if (argv.file){
